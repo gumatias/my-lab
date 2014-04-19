56d0601c0fba4c2ca3871efbcf8c3a413bf69818
Gustavo Matias <gustavo@matias.com>
2014-04-19 00:32:03 -0400

    user can now login, logout and register

--- picture-gallery/src/picture_gallery/models/db.clj
+++ picture-gallery/src/picture_gallery/models/db.clj
@@ -6,3 +6,14 @@
    :subname "//localhost/gallery"
    :user "admin"
    :password "admin"})
+
+(defmacro with-db [f & body]
+  `(sql/with-connection ~db (~f ~@body)))
+
+(defn create-user [user]
+  (with-db sql/insert-record :users user))
+
+(defn get-user [id]
+  (with-db sql/with-query-results 
+    res ["select * from users where id = ?" id] (first res)))
+
--- picture-gallery/src/picture_gallery/routes/auth.clj
+++ picture-gallery/src/picture_gallery/routes/auth.clj
@@ -4,28 +4,75 @@
             [picture-gallery.routes.home :refer :all]
             [picture-gallery.views.layout :as layout]
             [noir.session :as session]
-            [noir.response :as resp]))
+            [noir.response :as resp]
+            [noir.validation :as vali]
+            [noir.util.crypt :as crypt]
+            [picture-gallery.models.db :as db]))
+
+(defn format-error [id ex]
+  (cond 
+    (and (instance? org.postgresql.util.PSQLException ex)
+         (= 0 (.getErrorCode ex)))
+    (str "The user with id " id " already exists!")
+    :else 
+    "An error has occured while processing the request"))
+
+(defn valid? [id pass pass1]
+  (vali/rule (vali/has-value? id)
+             [:id "user id is required"])
+  (vali/rule (vali/min-length? pass 5)
+             [:pass "password must be at least 5 characters"])
+  (vali/rule (= pass pass1)
+             [:pass "entered password do not match"])
+  (not (vali/errors? :id :pass :pass1)))
+
+(defn error-item [[error]]
+  [:div.error error])
 
 (defn registration-page [& [id]]
-  (layout/common
+  (layout/base
     (form-to [:post "/register"]
+             (vali/on-error :id error-item)
              (label "user-id" "user id")
-             (text-field "id" id)
+             (text-field {:tabindex 1}  "id" id)
              [:br]
+             (vali/on-error :pass error-item)
              (label "pass" "password")
-             (password-field "pass")
+             (password-field {:tabindex 2} "pass")
              [:br]
+             (vali/on-error :pass1 error-item)
              (label "pass1" "retype password")
-             (password-field "pass1")
+             (password-field {:tabindex 3} "pass1")
              [:br]
-             (submit-button "create account"))))
+             (submit-button {:tabindex 4} "create account"))))
 
 (defn handle-registration [id pass pass1]
-  (session/put! :user id)
+  (if (valid? id pass pass1)
+    (try
+      (db/create-user {:id id :pass (crypt/encrypt pass)}) 
+      (session/put! :user id)
+      (resp/redirect "/")
+      (catch Exception ex
+        (vali/rule false [:id (format-error id ex)])
+        (registration-page)))
+    (registration-page id)))
+
+(defn handle-login [id pass]
+  (let [user (db/get-user id)]
+    (if (and user (crypt/compare pass (:pass user)))
+      (session/put! :user id)))
+  (resp/redirect "/"))
+
+(defn handle-logout []
+  (session/clear!)
   (resp/redirect "/"))
 
 (defroutes auth-routes 
   (GET "/register" []
        (registration-page))
   (POST "/register" [id pass pass1]
-        (handle-registration id pass pass1)))
+        (handle-registration id pass pass1))
+  (POST "/login" [id pass]
+        (handle-login id pass))
+  (GET "/logout" []
+       (handle-logout)))
