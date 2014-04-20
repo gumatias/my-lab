05b7507d3a8551be6083cc717a390851785294be
Gustavo Matias <gustavo@matias.com>
2014-04-20 13:25:45 -0400

    now with unit tests... I'm loving this sh*t!

--- picture-gallery/test/picture_gallery/test/handler.clj
+++ picture-gallery/test/picture_gallery/test/handler.clj
@@ -1,14 +1,32 @@
 (ns picture-gallery.test.handler
-  (:use clojure.test
-        ring.mock.request
-        picture-gallery.handler))
+  (:require [noir.util.crypt :refer [encrypt]]
+            [clojure.test :refer :all]
+            [ring.mock.request :refer :all]
+            [picture-gallery.handler :refer :all]))
+
+(defn mock-get-user [id]
+  (if (= id "foo")
+    {:id "foo" :pass (encrypt "12345")}))
+
+(deftest test-login
+  (testing "login success"
+    (with-redefs [picture-gallery.models.db/get-user mock-get-user]
+      (is 
+        (-> (request :post "/login" {:id "foo" :pass "12345"})
+            app :headers (get "Set-Cookie") not-empty))))
+  (testing "password mismatch"
+    (with-redefs [picture-gallery.models.db/get-user mock-get-user]
+      (is
+        (-> (request :post "/login" {:id "foo" :pass "123456"})
+            app :headers (get "Set-Cookie") empty?))))
+  (testing "user not found"
+    (with-redefs [picture-gallery.models.db/get-user mock-get-user]
+      (is 
+        (-> (request :post "/login" {:id "bar" :pass "12345"})
+            app :headers (get "Set-Cookie") empty?)))))
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
-
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
       (is (= (:status response) 404)))))
+
