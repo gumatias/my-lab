fab9aaf792684e50377cef9bb8b171303c72bb35
Gustavo Matias <gustavo@matias.com>
2014-09-19 16:57:35 -0400

    script that undo a particular coding convention

---
+++ undo_convention.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+path=$1
+# mFoo = pFoo; -> this.foo = foo;
+regex_for_constructor="%s/\([ \t]*\)[^this.]*m\([A-Z]\{1\}\)\([a-zA-Z0-9]\+\)[ ]*=[ ]*p\([A-Z]\{1\}\)\([a-zA-Z0-9]\+\);/\1this.\l\2\3 = \l\4\5;/g"
+# mFoo = foo; -> this.foo = foo;
+regex_for_setters="%s/\([\t]\{1,\}\|{ \)[^this.]*m\([A-Z]\{1\}\)\([a-zA-Z0-9]\+\)[ ]*=[ ]*\([a-z]\{2,\}[a-zA-Z0-9]*\);/\1this.\l\2\3 = \4;/g"
+# oFoo -> foo, pFoo -> foo, mFoo -> foo
+regex_for_all_fields="%s/\([^a-zA-Z]\)[opm]\{1\}\([A-Z]\{1\}\)\([a-zA-Z0-9]\+\)/\1\l\2\3/g"
+
+if [ -z $path ]
+then
+  echo "Please provide the file or directory as an argument"
+  exit 0
+fi
+
+undo_convention() {
+  vi -c "$regex_for_constructor" -c wq $1
+  vi -c "$regex_for_setters" -c wq $1
+  vi -c "$regex_for_all_fields" -c wq $1
+}
+
+if [ -d $path ]
+then
+  for f in `ls $path`
+  do
+    if [ ! -d "$path/$f" ]
+    then
+      undo_convention "$path/$f"
+    fi
+  done
+elif [ -e $path ]
+then
+  undo_convention $path
+fi
+
+echo "\nDone!\nPlease watch out for compilation errors or warnings in your files in case I messed up\n"
+
