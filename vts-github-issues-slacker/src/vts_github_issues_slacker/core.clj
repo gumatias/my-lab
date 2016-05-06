99511a7e0f02a11d5c77fa37e85fd12230740371
Gustavo Matias <gustavo@matias.com>
2016-05-06 07:58:04 -0400

    Now able to retrieve GitHub issues and Post to slack

--- vts-github-issues-slacker/src/vts_github_issues_slacker/core.clj
+++ vts-github-issues-slacker/src/vts_github_issues_slacker/core.clj
@@ -1,14 +1,23 @@
 (ns vts-github-issues-slacker.core
-  (:require [tentacles.search :as gh-search]))
+  (:require [tentacles.search :refer [search-issues]]
+            [clj-slack.chat :refer [post-message]]))
+
+; GITHUB
+(def github-access-token "")
 
 (defn assets-qa-issues
+  "Do not commit access token, or else GitHub will invalidate"
   []
-  (gh-search/search-issues
+  (search-issues
     ""
     {:repo "viewthespace/viewthespace" :label "qa" :state "open" :team "viewthespace/big-assets"}
-    {:access_token "ee282ebac13b3fcdc84670ca4683c4f3106e667d"}))
+    {:access_token github-access-token}))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; SLACK
+(def slack-access-token "")
+(def connection {:api-url "https://slack.com/api" :token slack-access-token})
+(def gus-test-group-id "G0KTFML3U")
+
+(defn post-qa-issues-on-assets-channel
+  []
+  (post-message connection gus-test-group-id "Testando galerinha do mal ^.^"))
