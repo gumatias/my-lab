d2d9ae69a9f390d8c14198654a454e3c8acdc92d
Gustavo Matias <gustavo@matias.com>
2016-05-09 04:45:10 -0400

    Remove project

--- vts-github-issues-slacker/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to vts-github-issues-slacker
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- vts-github-issues-slacker/test/vts_github_issues_slacker/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns vts-github-issues-slacker.core-test
-  (:require [clojure.test :refer :all]
-            [vts-github-issues-slacker.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
